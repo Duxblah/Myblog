5196815d22c6e4743a582107c741fd0260e1a739
KizeRemi <remi.mavillaz@live.fr>
2016-12-13 14:08:29 +0100

    add nbr comments

--- model/article.php
+++ model/article.php
@@ -1,13 +1,16 @@
 <?php
 
 function selectAllArticles () {
-	return select('article', ['article.*, user.pseudo as user'], 'JOIN user ON user.id = article.id_user ORDER BY created DESC');
+	return select('article', ['article.*, user.pseudo as user, count(comment.id) as nbr'], 'JOIN user ON user.id = article.id_user LEFT JOIN comment ON article.id = comment.id_article GROUP BY article.id ORDER BY article.created DESC ');
 }
 
 function selectArticleById ($id) {
 	return select('article', ['article.*, user.pseudo as user'], 'JOIN user ON user.id = article.id_user WHERE article.id = ' . $id)[0];
 }
 
+function selectArticleIdByCommeny ($id) {
+	return select('article', ['article.id'], 'JOIN comment ON comment.id = article.id_user WHERE article.id = ' . $id)[0];
+}
 function selectArticleByTitle ($title) {
 	return select('article', [], 'WHERE title = "' . $pseudo . '"')[0];
 }
--- views/list_articles.php
+++ views/list_articles.php
@@ -8,12 +8,12 @@
 			}
 		?>
 		<h3><a href="?p=single_article&id=<?= $article['id'] ?>"><?= $article['title']; ?></a></h3>
-		<div class="article_resume"><?= $content; ?></a></div>
+		<div class="article_resume"><?= $content; ?><a href="?p=single_article&id=<?= $article['id'] ?>"><span class="see_more">En savoir plus.</a> </span></div>
 		<h4>Informations sur l'article</h4>
 		<div class="article_info" >
 			<div class="article_info_author">Ecrit par <a class="link_author" href="?p=single_user&id=<?= $article['id_user'] ?>"><?= $article['user']; ?></a></div>
 			<div class="article_info_created">Le <a class="link_date" href="?p=single_user&id=<?= $article['id_user'] ?>"><?= $article['created']; ?></a></div>
-			<div class="article_info_comments">Commentaires (0)</div>
+			<div class="article_info_comments"><?php echo "Commentaires (".$article['nbr'].")"; ?></div>
 		</div>
 		<hr>
 	<?php endforeach; ?>
