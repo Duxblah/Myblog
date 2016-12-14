776df6589965e39b78da82512728e83dcbf98f99
KizeRemi <remi.mavillaz@live.fr>
2016-12-14 20:03:58 +0100

    style

--- views/single_user.php
+++ views/single_user.php
@@ -3,8 +3,8 @@
 <?php if (isset($_SESSION['user']) && $user['id'] == $_SESSION['user']) { ?>
 	<div id="delete_user"><a href="?p=delete_user&id=<?= $user['id'] ?>"><i class="fa fa-times" aria-hidden="true"></i>Supprimer mon compte </a></div>
 <?php } ?>
-
-<div></div>
+if()
+<h2></h2>
 
 <table id="list-articles">
 	<thead>
@@ -12,7 +12,8 @@
 			<th>Title</th>
 			<th>Content</th>
 			<?php if (isset($_SESSION['user']) && $user['id'] == $_SESSION['user']) { ?>
-				<th>Actions</th>
+				<th>Modifier</th>
+				<th>Supprimer</th>
 			<?php } ?>
 		</tr>
 	</thead>
@@ -28,7 +29,9 @@
 				<td><a href="?p=single_article&id=<?= $article['id'] ?>"><?= $article['title']; ?></a></td>
 				<td><a href="?p=single_article&id=<?= $article['id'] ?>"><?= $content; ?></a></td>
 				<?php if (isset($_SESSION['user']) && $user['id'] == $_SESSION['user']) { ?>
-					<td><a href="?p=modify_article&id=<?= $article['id'] ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Modifier </a><a href="?p=delete_article&id=<?= $article['id'] ?>"><i class="fa fa-times" aria-hidden="true"></i>Supprimer </a></td>
+					<td class="fa_single_user"><a href="?p=modify_article&id=<?= $article['id'] ?>"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></td>
+
+					<td class="fa_single_user"><a href="?p=delete_article&id=<?= $article['id'] ?>"><i class="fa fa-times" aria-hidden="true"></i></a></td>
 				<?php } ?>
 			</tr>
 		<?php endforeach; ?>
